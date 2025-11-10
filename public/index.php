ff73d5758c5766b4d5d66f49db4796356e286cd8
Bagus Manuto <kepo5524@gmail.com>
2025-11-10 14:53:28 +0700

    update first

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Http\Kernel;
+
+define('LARAVEL_START', microtime(true));
+
+// Maintenance mode
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Composer autoload
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+// Handle the request
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
